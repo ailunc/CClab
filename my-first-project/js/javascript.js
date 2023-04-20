dda8b6670e19f6a1f2379748f525fc21efc270ee
ailunc <mz3487@nyu.edu>
2023-04-20 11:58:55 +0800

    Initial commit

---
+++ my-first-project/js/javascript.js
@@ -0,0 +1,9 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(220);
+    fill(random(255),random(255),random(255),50)
+    circle(random(400),random(400),random(30,80))
+  }
